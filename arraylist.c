582ec3bd8e557bb27f66153d9ed52ca76491dd74
HasinJ <hasinchoudhuryj@gmail.com>
2021-02-21 01:06:19 -0500

    added insert function, only new length size

--- arraylist.c
+++ arraylist.c
@@ -6,8 +6,7 @@
 #define DEBUG 0
 #endif
 
-int al_init(arraylist_t *L, size_t length)
-{
+int al_init(arraylist_t *L, size_t length){
     L->data = malloc(sizeof(int) * length);
     if (!L->data) return 1;
 
@@ -17,17 +16,15 @@ int al_init(arraylist_t *L, size_t length)
     return 0;
 }
 
-void al_destroy(arraylist_t *L)
-{
+void al_destroy(arraylist_t *L){
     free(L->data);
 }
 
-
-int al_append(arraylist_t *L, int item)
-{
+int al_append(arraylist_t *L, int item){ //adds to the end
     if (L->used == L->length) {
     	size_t size = L->length * 2;
     	int *p = realloc(L->data, sizeof(int) * size);
+
     	if (!p) return 1;
 
     	L->data = p;
@@ -43,13 +40,21 @@ int al_append(arraylist_t *L, int item)
 }
 
 
-int al_remove(arraylist_t *L, int *item)
-{
+int al_remove(arraylist_t *L, int *item){
     if (L->used == 0) return 1;
-
     --L->used;
-
     if (item) *item = L->data[L->used];
-
     return 1;
+}
+
+int al_insert(arraylist_t *L, int index, int item){
+  if (L->length<index){
+    size_t new_length;
+    if(L->length*2<index) new_length=index;
+    else new_length=L->length;
+    if (DEBUG) printf("new_length %ld\n", new_length);
+
+    
+  }
+  return 1;
 }
