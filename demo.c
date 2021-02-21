57a0579b59bf588a607de6d3e658f1ade4b3a32d
HasinJ <hasinchoudhuryj@gmail.com>
2021-02-21 01:07:53 -0500

    added choices

--- demo.c
+++ demo.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include "arraylist.h"
 
-int cmp(const void *a, const void *b)
-{
+int cmp(const void *a, const void *b){
     int x = *(int*)a;
     int y = *(int*)b;
     if (x < y) return -1;
@@ -11,20 +10,37 @@ int cmp(const void *a, const void *b)
     return 1;
 }
 
-int main(int argc, char **argv)
-{
-    int d, i;
+int main(int argc, char **argv){
+    if (argc>2 || argc==1) {
+      printf("Please enter 1 file name.\n");
+      return EXIT_SUCCESS;
+    }
+
+    FILE *f;
+    f = fopen(argv[1],"r");
+    if (f==0) {
+      printf("error\n");
+      return EXIT_SUCCESS;
+    }
+
+
+    char choice[2];
+    int i, item;
     arraylist_t A;
     al_init(&A, 2);
 
-    while (1 == scanf("%d", &d)) {
-	al_append(&A, d);
+    while(fscanf(f,"%s %d",choice, &item)!=EOF){
+    	//al_append(&A, d);
+      if(choice[0]=='a') al_append(&A, item);
+      if(choice[0]=='i') printf("%d\n", item);
+      //printf("choice: %s, item: %d\n", choice, item);
     }
 
     qsort(A.data, A.used, sizeof(int), cmp);
 
+
     for (i = 0; i < A.used; ++i) {
-	printf("%d\n", A.data[i]);
+    	printf("%d\n", A.data[i]);
     }
 
     al_destroy(&A);
